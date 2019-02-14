8149b0d1de6be3f8a3210a2b50fe4c6ba5aebe45
hugh <yaohui@difengshanguo.com>
2019-02-14 11:22:57 +0800

    update package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { environment } from '../environments/environment';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { FormsModule , ReactiveFormsModule } from '@angular/forms';
-import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
@@ -26,10 +25,9 @@ registerLocaleData(en);
     ReactiveFormsModule,
     BrowserModule.withServerTransition({appId: 'my-app'}),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    NgZorroAntdModule,
     BrowserAnimationsModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
