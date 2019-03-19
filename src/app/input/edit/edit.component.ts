fe172b9ce64e37fc182abce2aeb5dc33c2f47996
hugh <yaohui@difengshanguo.com>
2019-03-19 17:15:54 +0800

    去除js引用

--- src/app/input/edit/edit.component.ts
+++ src/app/input/edit/edit.component.ts
@@ -64,13 +64,8 @@ export class EditComponent implements OnInit, AfterViewInit {
       { text: 'HTML/XML', value: 'markup' },
       { text: 'JavaScript', value: 'javascript' },
       { text: 'CSS', value: 'css' },
-      { text: 'PHP', value: 'php' },
-      { text: 'Ruby', value: 'ruby' },
-      { text: 'Python', value: 'python' },
-      { text: 'Java', value: 'java' },
-      { text: 'C', value: 'c' },
-      { text: 'C#', value: 'csharp' },
-      { text: 'C++', value: 'cpp' }
+      { text: 'TypeScript', value: 'typescript' },
+      { text: 'Java', value: 'java' }
     ]
   };
   constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) { }
