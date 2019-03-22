3391899f95c5707f5837d5c5a8e3d66ba5329909
hugh <yaohui@difengshanguo.com>
2019-03-22 11:36:09 +0800

    高亮详情页代码

--- src/app/main/detail/detail.component.ts
+++ src/app/main/detail/detail.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { baseInfo } from '../database/baseInfo';
 import { HttpClient } from '@angular/common/http';
 import { Articel } from '../../data-model/article-model';
 import { switchMap } from 'rxjs/operators';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import * as hljs from 'highlight.js';
+import * as Prism from '../../../assets/js/prism.js';
 interface ArticleDetail {
   title: String;
   content: String;
@@ -21,32 +22,21 @@ interface ArticleDetail {
   styleUrls: ['./detail.component.scss', './default.css']
 })
 export class DetailComponent implements OnInit, AfterViewInit {
-  @ViewChild('code') codeElement: ElementRef;
-
-  languages = ['html', 'typescript'];
-
   detail: ArticleDetail;
-  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) { }
-
-  ngOnInit() {
-    // this.route.paramMap.pipe(
-    //   switchMap((params: ParamMap) =>
-    //     params.get('id')
-    //   )
-    // ).subscribe((val) => {
-    //   console.log(val);
-    this.http.get('/api/article/get?title=' + this.route.paramMap.source['value']['title']).subscribe(res => {
-      hljs.highlightAuto(res['data'][0].content);
-      this.detail = res['data'][0];
-      hljs.highlightBlock(this.codeElement.nativeElement);
-    });
-    // })
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) {
 
   }
+
+  ngOnInit() {
+    this.http.get('/api/article/get?title=' + this.route.paramMap.source['value']['title']).subscribe(res => {
+      this.detail = res['data'][0];
+    });
+  }
   ngAfterViewInit(): void {
-    hljs.highlightBlock(this.codeElement.nativeElement);
-    // this.codeElement.nativeElement.textContent = 'console.log(b)';
-    // hljs.highlightBlock(this.codeElement.nativeElement);
+    // this.el.nativeElement.querySelector('#code');
+    Prism.highlightAll();
+    // console.log(this.el.nativeElement.querySelector('.code'));
+    // Prism.highlightElement(this.el.nativeElement.querySelector('#code')[0]);
   }
   edit() {
     this.router.navigate(['/input/edit', this.detail.title]);
